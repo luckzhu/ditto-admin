b2a226e125ade92e14cade14d458d1b34f8a86f3
luckzhu <luckzhu1992@gmail.com>
2019-12-10 16:02:31 +0800

    add  icon component

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <svg :class="svgClass">
+    <use :xlink:href="iconFullName" aria-hidden="true" />
+  </svg>
+</template>
+
+<script>
+/** 
+ * iconName 为 icon 的名字，必传
+ *  iconClass 为 icon 自定义 class
+ */
+export default {
+  name: "SvgIcon",
+  props: {
+    iconName: {
+      type: String,
+      required: true
+    },
+    iconClass: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    iconFullName() {
+      return `#icon-${this.iconName}`
+    },
+    svgClass() {
+      if (this.iconClass) {
+        return 'svg-icon' + this.iconClass
+      } else {
+        return 'svg-icon'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
