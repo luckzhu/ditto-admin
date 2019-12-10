0a31459ef64b7dd3474e846586f399cd7b32a32c
luckzhu <luckzhu1992@gmail.com>
2019-12-10 17:20:54 +0800

    add layout structure

---
+++ src/layout/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <sidebar />
+    <navbar />
+    <router-view>
+      <app-main />
+    </router-view>
+  </div>
+</template>
+
+<script>
+import { Sidebar,AppMain,Navbar } from './components/index.js'
+export default {
+  components: {
+    Sidebar,
+    AppMain,
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,31 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Layout from "@/layout/index.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
+const constantRoutes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    component: Layout,
+    redirect: "/home",
+    children: [
+      {
+        path: "home",
+        name: "Home",
+        component: () => import("@/views/Home.vue")
+      }
+    ]
   },
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import("../views/About.vue")
   }
 ];
 
 const router = new VueRouter({
-  routes
+  routes: constantRoutes
 });
 
 export default router;
