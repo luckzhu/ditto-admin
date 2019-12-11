01101ae12612bf737b6bad31c4c560b5dbbe20a6
luckzhu <luckzhu1992@gmail.com>
2019-12-11 10:23:28 +0800

    add normalize.css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 
+import "normalize.css/normalize.css"; // a modern alternative to CSS resets
+
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 
