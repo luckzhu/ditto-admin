a5809f39faf6981d13fb344b169a4f71c310e7ef
luckzhu <luckzhu1992@gmail.com>
2019-12-30 11:24:57 +0800

    complete headsearch

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -5,15 +5,21 @@
     </div>
 
     <el-select
-      v-model="value"
+      v-model="search"
       class="el-select-search"
       filterable
       remote
       :remote-method="remoteMethod"
       :loading="loading"
       placeholder="Search"
+      @change="change"
     >
-      <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+      <el-option
+        v-for="item in options"
+        :key="item.path"
+        :label="item.title.join(' > ')"
+        :value="item"
+      ></el-option>
     </el-select>
   </div>
 </template>
@@ -26,80 +32,110 @@ export default {
   data() {
     return {
       isSelectShow: false,
-      value: '',
+      search: '',
       loading: false,
-      options: [
-        {
-          value: '选项1',
-          label: '黄金糕'
-        },
-        {
-          value: '选项2',
-          label: '双皮奶'
-        },
-        {
-          value: '选项3',
-          label: '蚵仔煎'
-        },
-        {
-          value: '选项4',
-          label: '龙须面'
-        },
-        {
-          value: '选项5',
-          label: '北京烤鸭'
-        }
-      ]
+      options: [],
+      list: [],
+      searchPool: [],
+      fuse: ''
+    }
+  },
+  computed: {
+    routes() {
+      return this.$store.getters.permission_routes
+    }
+  },
+  watch: {
+    routes(val) {
+      this.searchPool = this.generateOptions(val)
+    },
+    searchPool(list) {
+      this.initFuse(list)
     }
   },
   mounted() {
-    this.generateOptions(this.$store.getters.permission_routes)
+    this.searchPool = this.generateOptions(this.routes)
   },
   methods: {
+    change(val) {
+      this.$router.push(val.path)
+      this.search = ''
+      this.options = []
+      this.$nextTick(() => {
+        this.isSelectShow = false
+      })
+    },
     toggleSelect() {
       this.isSelectShow = !this.isSelectShow
     },
+    // 过滤掉可以显示在侧栏中的路由，并生成国际化的标题
     generateOptions(routes, basePath = '/', prefixTitle = []) {
       let res = []
-      routes.forEach(route => {
-        if (route.hidden) {
-          return
+
+      for (const router of routes) {
+        // 跳过隐藏路由, continue 控制程序跳转到指定循环的下一次迭代，而非当前循环
+        if (router.hidden) {
+          continue
         }
         const data = {
-          title: [...prefixTitle],
-          path: path.resolve(basePath, route.path)
+          /**
+           * path.resolve方法用于将相对路径转为绝对路径。
+           * 它可以接受多个参数，依次表示所要进入的路径，直到将最后一个参数转为绝对路径。
+           * 如果根据参数无法得到绝对路径，就以当前所在路径作为基准。
+           * 除了根目录，该方法的返回值都不带尾部的斜杠。
+           */
+          path: path.resolve(basePath, router.path),
+          title: [...prefixTitle]
         }
-        if (route.meta && route.meta.title) {
-          data.title = [...prefixTitle, route.meta.title]
-          if (route.redirect !== 'noRedirect') {
+
+        if (router.meta && router.meta.title) {
+          data.title = [...data.title, router.meta.title]
+
+          if (router.redirect !== 'noRedirect') {
+            // only push the routes with title
+            // 需要排除父路由没有设置 redirect
             res.push(data)
           }
         }
 
-        if (route.children) {
+        // 递归子路由
+        if (router.children) {
           const tempRoutes = this.generateOptions(
-            route.children,
+            router.children,
             data.path,
             data.title
           )
-
-          if (tempRoutes && tempRoutes.length >= 1) {
-            res = [...res, tempRoutes]
+          if (tempRoutes.length >= 1) {
+            res = [...res, ...tempRoutes]
           }
         }
-      })
-      console.log(res)
+      }
       return res
+    },
+    initFuse(list) {
+      this.fuse = new Fuse(list, {
+        shouldSort: true, // 需要排序
+        threshold: 0.4, // 在什么情况下匹配算法会放弃。0.0的阈值需要完全匹配(字母和位置)，1.0的阈值可以匹配任何内容。
+        location: 0, // 大致确定文本中期望找到的模式的位置
+        distance: 100, // 确定匹配必须与模糊位置(由位置指定)的距离
+        maxPatternLength: 32, // 模式越长(即搜索查询)，搜索操作就越密集。
+        minMatchCharLength: 1, // 最低匹配字符长度
+        keys: [
+          {
+            //要搜索的属性列表。支持嵌套属性、加权搜索、在字符串和对象数组中搜索
+            name: 'title',
+            weight: 0.7
+          },
+          {
+            name: 'path',
+            weight: 0.3
+          }
+        ]
+      })
     },
     remoteMethod(query) {
       if (query !== '') {
-        this.loading = true
-        setTimeout(() => {
-          this.loading = false
-          this.options = this.list.filter(item => {
-            return item.label.toLowerCase().indexOf(query.toLowerCase()) > -1
-          })
-        }, 200)
+        this.options = this.fuse.search(query)
       } else {
         this.options = []
       }
